2fe04322d923c2acce7f1f3afb08bfc111436f54
Pervuhin Roman <rg.pervuhin5198@gmail.com>
2021-11-06 16:57:20 +0500

    Add files via upload

--- Работа с обыкновенной дробью/Main.java
+++ Работа с обыкновенной дробью/Main.java
@@ -4,50 +4,86 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int numerator(String fraction){
+    public static int Numerator(String fraction){
         String subStr [] = fraction.split("/", 2);
         int wholePart = Integer.parseInt(subStr[0]);
         return wholePart;
     }
 
-    public static int denominator(String fraction){
+    public static int Denominator(String fraction){
         String subStr [] = fraction.split("/", 2);
         int fractionalPart = Integer.parseInt(subStr[1]);
         return fractionalPart;
     }
 
-    public static void main(String[] args) {
+    public static void MainVariant1(){
         Scanner in = new Scanner(System.in);
-        while (true){
-            System.out.println();
-            System.out.println("Enter the fraction: ");
 
-            String frac1 = in.nextLine();
-            Fraction fraction1 = new Fraction(numerator(frac1), denominator(frac1));
+        System.out.print("Enter the first fraction: ");
+        String frac1 = in.nextLine();
+        Fraction fraction1 = new Fraction(Numerator(frac1), Denominator(frac1));
 
-            String action = in.nextLine();
+        System.out.print("Enter the action: ");
+        String action = in.nextLine();
 
-            String frac2 = in.nextLine();
-            Fraction fraction2 = new Fraction(numerator(frac2), denominator(frac2));
+        System.out.print("Enter the second fraction: ");
+        String frac2 = in.nextLine();
+        Fraction fraction2 = new Fraction(Numerator(frac2), Denominator(frac2));
 
-            switch (action){
-                case "+":
-                    Fraction newFrac0 = new Fraction(1, 1).SumFractions(fraction1, fraction2);
-                    System.out.println(newFrac0.Print());
-                    break;
-                case "-":
-                    Fraction newFrac1 = new Fraction(1, 1).DifferenceFractions(fraction1, fraction2);
-                    System.out.println(newFrac1.Print());
-                    break;
-                case ":":
-                    Fraction newFrac2 = new Fraction(1, 1).QuotientFractions(fraction1, fraction2);
-                    System.out.println(newFrac2.Print());
-                    break;
-                case "*":
-                    Fraction newFrac3 = new Fraction(1, 1).ProductFractions(fraction1, fraction2);
-                    System.out.println(newFrac3.Print());
-                    break;
-            }
+        switch (action){
+            case "+":
+                Fraction newFrac0 = new Fraction(1, 1).SumFractions(fraction1, fraction2);
+                System.out.println(newFrac0.Print());
+                break;
+            case "-":
+                Fraction newFrac1 = new Fraction(1, 1).DifferenceFractions(fraction1, fraction2);
+                System.out.println(newFrac1.Print());
+                break;
+            case ":":
+                Fraction newFrac2 = new Fraction(1, 1).QuotientFractions(fraction1, fraction2);
+                System.out.println(newFrac2.Print());
+                break;
+            case "*":
+                Fraction newFrac3 = new Fraction(1, 1).ProductFractions(fraction1, fraction2);
+                System.out.println(newFrac3.Print());
+                break;
         }
+    }
+
+    public static void MainVariant2(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first fraction: ");
+        String frac1 = in.nextLine();
+        Fraction fraction1 = new Fraction(Numerator(frac1), Denominator(frac1));
+
+        System.out.print("Enter the action: ");
+        String action = in.nextLine();
+
+        System.out.print("Enter the second fraction: ");
+        String frac2 = in.nextLine();
+        Fraction fraction2 = new Fraction(Numerator(frac2), Denominator(frac2));
+
+        System.out.print("Result: ");
+
+        switch (action){
+            case "+":
+                System.out.println(fraction1.SumFractionTo(fraction2).Print());
+                break;
+            case "-":
+                System.out.println(fraction1.DifferenceFractionFrom(fraction2).Print());
+                break;
+            case ":":
+                System.out.println(fraction1.QuotientThisFractionsTo(fraction2).Print());
+                break;
+            case "*":
+                System.out.println(fraction1.ProductThisFractionTo(fraction2).Print());
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        // MainVariant1();
+        MainVariant2();
     }
 }
