46667ead86deb31d019824b28a7fc6e533ab6ac0
Pervuhin Roman <rg.pervuhin5198@gmail.com>
2021-12-25 22:41:47 +0500

    Add files via upload

--- JavaCalc/MainActivity.java
+++ JavaCalc/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.calcapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "Main_Activity";
 
     Button btnAdd;
     Button btnMul;
@@ -23,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Creating view..");
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "View created!");
 
         btnAdd = (Button) findViewById(R.id.add);
         btnMul = (Button) findViewById(R.id.mul);
@@ -38,38 +43,56 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int arg1 = Integer.parseInt(argument1.getText().toString());
-                int arg2 = Integer.parseInt(argument2.getText().toString());
-                answer.setText(Integer.toString(arg1 + arg2));
-
-                // TODO add try-catch in all method onClick
+                try {
+                    answer.setText(Integer.toString(Integer.parseInt(argument1.getText().toString()) + Integer.parseInt(argument2.getText().toString())));
+                } catch (NumberFormatException e){
+                    answer.setText("Input Error");
+                    Log.d(LOG_TAG, e.getMessage());
+                    Log.d(LOG_TAG, e.toString());
+                }
             }
         });
 
         btnMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int arg1 = Integer.parseInt(argument1.getText().toString());
-                int arg2 = Integer.parseInt(argument2.getText().toString());
-                answer.setText(Integer.toString(arg1*arg2));
+                try {
+                    answer.setText(Integer.toString(Integer.parseInt(argument1.getText().toString()) * Integer.parseInt(argument2.getText().toString())));
+                } catch (ArithmeticException e){
+                    answer.setText("Div by zero");
+                    Log.d(LOG_TAG, e.getMessage());
+                    Log.d(LOG_TAG, e.toString());
+                }
             }
         });
 
         btnDivide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int arg1 = Integer.parseInt(argument1.getText().toString());
-                int arg2 = Integer.parseInt(argument2.getText().toString());
-                answer.setText(Integer.toString(arg1 / arg2));
+                try {
+                    answer.setText(Integer.toString(Integer.parseInt(argument1.getText().toString()) / Integer.parseInt(argument2.getText().toString())));
+                } catch (ArithmeticException e){
+                    answer.setText("Div by zero");
+                    Log.d(LOG_TAG, e.getMessage());
+                    Log.d(LOG_TAG, e.toString());
+                } catch (NumberFormatException e){
+                    answer.setText("Input Error");
+                    Log.d(LOG_TAG, e.getMessage());
+                    Log.d(LOG_TAG, e.toString());
+                }
             }
         });
 
         btnSubtr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int arg1 = Integer.parseInt(argument1.getText().toString());
-                int arg2 = Integer.parseInt(argument2.getText().toString());
-                answer.setText(Integer.toString(arg1 - arg2));
+                try {
+                    answer.setText(Integer.toString(Integer.parseInt(argument1.getText().toString()) - Integer.parseInt(argument2.getText().toString())));
+                } catch (ArithmeticException e){
+                    answer.setText("Div by zero");
+                    Log.d(LOG_TAG, e.getMessage());
+                    Log.d(LOG_TAG, e.toString());
+                }
             }
         });
     }
